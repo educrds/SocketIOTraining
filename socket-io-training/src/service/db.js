a08db7ef6bd9be9d63eefe45e947d6159224c10a
eduardo <eduardocardoso1039@gmail.com>
2023-09-02 15:31:26 -0300

    added environment variables

--- socket-io-training/src/service/db.js
+++ socket-io-training/src/service/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'fastcheck',
-  user: 'postgres',
-  password: 'password',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 const result = pool.query('SELECT * FROM tb_genero');
